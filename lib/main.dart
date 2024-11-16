55a80948b59d540c249f2211a9ccbe326aec2ac6
oozuser01 <sanadzedan434@gmail.com>
2024-11-16 09:31:36 +0200

    DB

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:untitled/Utils/Utils.dart';
 import 'package:untitled/Views/RegisterScreen.dart';
 
+import 'Utils/DB.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,13 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -27,8 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
-
   final String title;
 
   @override
@@ -37,33 +35,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final TextEditingController _txtEmail=TextEditingController();
-  final TextEditingController _txtpassword=TextEditingController();
-  final TextEditingController _txtFirstname=TextEditingController();
+  final TextEditingController _txtEmail = TextEditingController();
+  final TextEditingController _txtpassword = TextEditingController();
+  final TextEditingController _txtFirstname = TextEditingController();
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('first name',),
+            Text(
+              'first name',
+            ),
             TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
@@ -71,7 +65,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               controller: _txtFirstname,
             ),
-            Text('Email',),
+            Text(
+              'Email',
+            ),
             TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
@@ -79,7 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               controller: _txtEmail,
             ),
-            Text('password',),
+            Text(
+              'password',
+            ),
             TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
@@ -87,30 +85,32 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               controller: _txtpassword,
             ),
-
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(onPressed: () {
-                  Utils uti = new Utils();
-                  uti.showMyDialog(context,_txtFirstname.text,_txtEmail.text);
-                },
+                ElevatedButton(
+                    onPressed: () {
+                      insertUser('sanad', '6723176998642986', 'zedan');
+                      Utils uti = new Utils();
+                      uti.showMyDialog(context, _txtFirstname.text, _txtEmail.text);
+                      // insertUser("firstName", "secondName", "passWord");
+                    },
                     child: Text("register")),
-
-                ElevatedButton(onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegisterScreen()),
-                  );
-                }, child: Text("sign up"))
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => RegisterScreen()),
+                      );
+                    },
+                    child: Text("sign up"))
               ],
             ),
-
           ],
         ),
       ),
-
     );
   }
 }
