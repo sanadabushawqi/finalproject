93b37fc5c8ddc28fe68acc980eec0fcfc3d8f640
oozuser01 <sanadzedan434@gmail.com>
2024-11-19 12:55:27 +0200

    student

---
+++ lib/Models/Student.dart
@@ -0,0 +1,39 @@
+class Student{
+  String studentID;
+  String firstName;
+  String lastName;
+  String phone;
+  String ID;
+  String birthDate;
+  String email;
+
+  Student({
+    this.studentID="",
+    this.firstName="",
+    this.lastName="",
+    this.phone="",
+    this.ID="",
+    this.birthDate="",
+    this.email="",
+  });
+
+  factory student.fromJson(Map<String, dynamic> json)=>Student(
+    studentID:json["studentID"],
+    firstName:json["firstName"],
+    lastName:json[" lastName"],
+    phone:json["phone"],
+    ID:json["ID"],
+    birthDate:json["birthDate"],
+    email:json["email"],
+  );
+
+  Map<String,dynamic>tojson()=>{
+    "studentID":studentID,
+    "firstName":firstName,
+    " lastName": lastName,
+    "phone":phone,
+    "ID":ID,
+    "birthDate":birthDate,
+    "email":email,
+  };
+}
